52f15c011083b43ae38575b0b3a8f0c33fe5b0c6
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-05 02:59:18 +0530

    Create am.py

---
+++ am.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("rm -rf spammerblockbot && git clone https://github.com/AbhiMod/spammerblockbot && cd spammerblockbot && python3 -m spr")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
