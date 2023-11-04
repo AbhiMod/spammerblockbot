84a4224b72a2ce4efa060c6b3fb82965c80cb2a7
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-05 04:11:23 +0530

    Update __init__.py

--- spr/__init__.py
+++ spr/__init__.py
@@ -5,7 +5,7 @@ from aiohttp import ClientSession
 from pyrogram import Client
 from Python_ARQ import ARQ
 
-SESSION_NAME = "BQChRclD6suwWBL-I4n9rD-K0Hgm7cQ_Ks98CrAvtaCRDznRr3ArNbunyjKT1TihOiwvQcXX-aSvZSy9uoxc-tBg4hPAnXH1E3wY5rb_Bf39OnNmXCNKCTnO8018hM6UdkURi2K1V2HbpDGgdl9D6duML4a8Jl52JpuwpQ10DejrQQDrB4qCPIWu8cgIEteWmg91IEFIfNLHrWK-sSVS0nuUmI1JwvOQZXt7fW5NmspO2EBAfAt2DF5p65pHpjrSdMKRNXaFH5afRXRGJjlGGJIFKrGmRoEEFmvZSkLc-s9jGlWe3_67_Rl5zZqZUrRgld5WtXdGt0zLQ8bZWa705oilAAAAAXHVJUAA"
+SESSION_NAME = "spr"
 DB_NAME = "db.sqlite3"
 API_ID = 27733303
 API_HASH = "c3c9d5e5d89c99fb8bb85a22a0cb5a26"
