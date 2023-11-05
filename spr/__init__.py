0abd84e915c9ab0d51716a660265b8e031a5a524
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-05 11:48:05 +0530

    Update __init__.py

--- spr/__init__.py
+++ spr/__init__.py
@@ -11,19 +11,6 @@ from aiohttp import ClientSession
 from pyrogram import Client, errors
 from telethon import TelegramClient
 
-StartTime = time.time()
-
-# enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-    level=logging.INFO,
-)
-
-logging.getLogger("apscheduler").setLevel(logging.ERROR)
-logging.getLogger("telethon").setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-LOGGER = logging.getLogger(__name__)
 
 
 SESSION_NAME = "spambot"
