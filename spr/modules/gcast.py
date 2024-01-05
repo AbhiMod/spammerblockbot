62b1a71e638b0534c0a6c2c025ecfbebff751e83
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2024-01-05 06:39:58 +0530

    Update gcast.py

--- spr/modules/gcast.py
+++ spr/modules/gcast.py
@@ -28,20 +28,10 @@ db = mongo.users
 db = db.users
 
 AM_PIC = [
-    "https://telegra.ph/file/7c25ef427c9f3cded5577.jpg",
-    "https://telegra.ph/file/625d235cc0a22fb8525b5.jpg",
-    "https://telegra.ph/file/1c62254d59baf7f968ba7.jpg",
-    "https://telegra.ph/file/7a0553bd4664486ab3008.jpg",
-    "https://telegra.ph/file/7b4dfa606e6f23961d30e.jpg",
-    "https://telegra.ph/file/2773dec98d87b8562618c.jpg",
-    "https://telegra.ph/file/80353d02e0368b71d2666.jpg",
-    "https://telegra.ph/file/6e5331dc4bef87464ea1c.jpg",
-    "https://telegra.ph/file/199a2e44cb8e77bb21b34.jpg",
-    "https://telegra.ph/file/8371bcd8952d089f9ec05.jpg",
-    "https://telegra.ph/file/f970e559dd1bb96fced1a.jpg",
-    "https://telegra.ph/file/59a305f8ce0c4e85949cc.jpg"
+    "https://graph.org/file/72157e2add09a703c8fea.jpg",
+    "https://graph.org/file/fe83c0e20b3ccd5bfa16a.jpg",
+   "https://graph.org/file/fd93b9bde07fff9fb91b5.jpg"
 ]
-
 #CHATGROUP
 
 async def get_chats():
@@ -245,6 +235,8 @@ def time_formatter(milliseconds):
 async def activevc(_, message: Message):
     uptime = time_formatter((time.time() - start_time) * 1000)
     cpu = psutil.cpu_percent()
+    users = len(await get_users())
+    chats = len(await get_chats())
     TEXT = f"**ᴜᴘᴛɪᴍᴇ** : {uptime} | **ᴄᴘᴜ** : {cpu}% | ᴄʜᴀᴛs : {chats}| ᴜsᴇʀs : {users}"
     await message.reply(TEXT)
     
