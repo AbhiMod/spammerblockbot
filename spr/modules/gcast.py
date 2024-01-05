f73bbd5cd6c65ed3249f40a42cb722b6504ff26a
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2024-01-05 05:54:45 +0530

    Update gcast.py

--- spr/modules/gcast.py
+++ spr/modules/gcast.py
@@ -12,6 +12,8 @@ from pyrogram.types import (
     InlineKeyboardMarkup,
     InlineKeyboardButton,
 )
+import psutil
+import time
 import random
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 
@@ -220,11 +222,43 @@ async def stats(cli: Client, message: Message):
 """
     )
 
+start_time = time.time()
+
+def time_formatter(milliseconds):
+    minutes, seconds = divmod(int(milliseconds / 1000), 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    weeks, days = divmod(days, 7)
+    tmp = (((str(weeks) + "ᴡ:") if weeks else "") +
+           ((str(days) + "ᴅ:") if days else "") +
+           ((str(hours) + "ʜ:") if hours else "") +
+           ((str(minutes) + "ᴍ:") if minutes else "") +
+           ((str(seconds) + "s") if seconds else ""))
+    if not tmp:
+        return "0s"
+    if tmp.endswith(":"):
+        return tmp[:-1]
+    return tmp
+
+
+@app.on_message(filters.command("bot"))
+async def activevc(_, message: Message):
+    uptime = time_formatter((time.time() - start_time) * 1000)
+    cpu = psutil.cpu_percent()
+    served_chats = len(await get_served_chats())
+    served_users = len(await get_served_users())
+    TEXT = f"**ᴜᴘᴛɪᴍᴇ** : {uptime} | **ᴄᴘᴜ** : {cpu}% "
+    await message.reply(TEXT)
+    
 __MODULE__ = "Stats"
 __HELP__ = """
 **The Cmds Is Bot Stats**
 
 /gcast - Reply With Messege
 
+/announce - Reply To Some Post To Broadcast
+
 /stats - Check Bot User Stast
+
+/bot - Bot Uptime To Load
 """
